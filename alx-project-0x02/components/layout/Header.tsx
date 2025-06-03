931c83a156d232bc5a204fd4514bc3e8dc5a0511
SAMAR KHALED <142172579+Samarmar2727@users.noreply.github.com>
2025-06-03 17:49:41 +0300

    add a new files

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,17 @@
+import Home from "@/pages/home";
+import About from "@/pages/about";
+
+export default function Header (){
+    return (
+        <>
+        <h1>welcome to  Home page </h1>
+        <nav>
+            <Home/>
+            <About/>
+
+        </nav>
+        
+        </>
+
+    )
+}
